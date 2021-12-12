41eb2f50d0ca5536d801492fb6274f639f835606
Matthew <78713317+mreed4@users.noreply.github.com>
2021-12-11 22:44:37 -0800

    More About tweaks, Contact content tweaks

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/*
+let discord = document.getElementById(`discord`);
+
+discord.addEventListener(`mouseover`, function () {
+  discord.textContent = `mreed#7521`;
+  discord.onclick = function() {
+    document.execCommand
+  }
+});
+
+discord.addEventListener(`mouseout`, function () {
+  discord.textContent = `Discord`;
+});
+*/
